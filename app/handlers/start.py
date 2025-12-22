470e777aeb33de015378e8d289f37369ece8935d
proyectoautomate9-cmd <proyectoautomate9@gmail.com>
2025-12-22 16:52:20 -0500

    feat(handlers): add start command handler skeleton

---
+++ app/handlers/start.py
@@ -0,0 +1,13 @@
+﻿"""
+Telegram Bot Handlers
+Handler para el comando /start
+"""
+
+async def start_handler(update, context):
+    """Handler para el comando /start"""
+    user = update.effective_user
+    await update.message.reply_text(
+        f'¡Hola {user.first_name}! Bienvenido a Milhoja Dres Bot.'
+    )
+
+# TODO: Implementar handlers completos
